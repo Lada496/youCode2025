76658caf439cd0360d653b4849443309b3c5ee1f
Lada496 <67321065+Lada496@users.noreply.github.com>
2025-04-05 14:42:40 -0700

    added user-auth with no styling

--- hiking-app/src/app/login/page.tsx
+++ hiking-app/src/app/login/page.tsx
@@ -1,7 +1,13 @@
+import { login, signup } from "./actions";
 export default function LoginPage() {
   return (
-    <div>
-        Login Page
-    </div>
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
   );
 }
--- hiking-app/src/app/profile/page.tsx
+++ hiking-app/src/app/profile/page.tsx
@@ -1,7 +1,10 @@
-export default function ProfilePage() {
-  return (
-    <div>
-        Login Page
-    </div>
-  );
+import EditProfileForm from "./edit-profile-form";
+import { createClient } from "../utils/supabase/server";
+
+export default async function Profile() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return <EditProfileForm user={user} />;
 }
